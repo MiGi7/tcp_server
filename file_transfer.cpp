33ec63db5d993cf1531c68e48fc244bdf5d8e74d
Michael <mike.m7@hotmail.com>
2019-06-24 18:08:32 -0400

    constructors created for file and packet

--- file_transfer.cpp
+++ file_transfer.cpp
@@ -1,1 +1,28 @@
 #include "file_transfer.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+Packet::Packet(char buffer[4096], int bytes, bool end){
+  buffer[4096] = buffer;
+  bytes = bytes;
+  end = end;
+}
+
+//need to define function so that a file_name will create the File object containing all packets
+File::File(std::string file_name){
+  file_name = file_name;
+
+}
+
+//need to push packet onto the back of the vector array so
+//that the end packet is at the back
+int File::pushPacket(Packet packet){
+  packets.push_back(packet);
+  return 1;
+}
+
+int fileToBinary(std::string file_name){
+
+}
--- file_transfer.h
+++ file_transfer.h
@@ -1,5 +1,38 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
 #ifndef file_transfer_H
 #define file_transfer_H
 
+//class designed for single packages of data sent via tcp
+//4095 bytes are sent to reduce data loss
+class Packet {
+public:
+  char buffer[4096];
+  int bytes;
+  bool end;
+
+  Packet(char buffer[4096], int bytes  bool end=false);
+};
+
+class File {
+public:
+  std::vector<Packet> packets;
+  int total_bytes;
+  int total_packets;
+  const std::string file_name;
+
+  File(std::string filename);
+
+  int pushPacket(Packet packet);
+
+  int fileToBinary();
+
+  int binaryToFile(std::string file_name);
+  //bool check();
+
+}
 
 #endif
